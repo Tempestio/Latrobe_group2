a17f8eda925954e620021bc68695b1eff17bb4f8
Tempestio <102910238+Tempestio@users.noreply.github.com>
2022-04-10 18:52:13 +1000

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,1 +1,29 @@
-<>
+<?php
+$userName = $_POST['userName'];
+$userPassword = $_POST['userPassword'];
+
+
+$con = new mysqli('database-2.c4fj7nfsjfzd.ap-southeast-2.rds.amazonaws.com', 'admin','adminadmin', 'Passwords');
+	if($con->connect_error){
+		die("Failed to connect : ".$con->connect_error);
+
+    }
+    else {
+
+        $stmt = $con->prepare("SELECT * FROM Passwords WHERE userName = ?");
+        $stmt ->bind_param("s", $userName);
+        $stmt->execute();
+        $stmt_result = $stmt->get_result();
+        if ($stmt_result->num_rows > 0) {
+
+            if ($stmt_result->num_rows > 0) {
+                $data = $stmt_result->fetch_assoc();
+                if($data['userPassword'] === $userPassword) {
+                    echo "<h2>Login Successful<h2>";
+            } else {
+                echo "<h2>Invalid Email or Password</h2>";
+            }
+        }
+    }   
+}
+?>
